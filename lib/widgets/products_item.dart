dedbed52bf35caa440dc11ebd71ef26c72b91249
anshu06468 <anshukumar.kumar447@gmail.com>
2021-05-14 15:28:33 +0530

    GridTile added

---
+++ lib/widgets/products_item.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String imgUrl;
+  final String id;
+  final String title;
+
+  ProductItem(this.imgUrl, this.id, this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          imgUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.favorite,
+              color: Theme.of(context).accentColor,
+            ),
+          ),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+              color: Theme.of(context).accentColor,
+            ),
+            onPressed: () {},
+          ),
+          title: Text(
+            title,
+            style: TextStyle(
+              fontSize: 12,
+            ),
+            overflow: TextOverflow.visible,
+            softWrap: true,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
